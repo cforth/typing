638a68628146655949b92335cd1a9826e74306d5
ChaiFei <cforth@163.com>
2015-11-15 18:59:06 +0800

    简化代码

--- public/js/typing.js
+++ public/js/typing.js
@@ -4,12 +4,10 @@
 
 //创建成绩统计对象
 var counter = {
-    num: 0,
-    backNum: 0,
-    times: 0,
-    inc: function() {
-        this.times += 1;
-    }
+    num: 0,                //正确的字符总数
+    backNum: 0,            //退格数
+    errorNum: 0,           //错字数
+    times: 0,              //耗费的时间（单位为秒）
 };
 
 
@@ -40,12 +38,6 @@ function update_counter(key_num, c) {
 }
 
 
-//更新文章对象中的当前字符
-function update_articles(num) {
-    articles.index = num - 1;
-}
-
-
 //比较两个字符是否相同
 function compare_key(keyboard_key, articles_key) {
     if(keyboard_key == articles_key) {
@@ -58,15 +50,15 @@ function compare_key(keyboard_key, articles_key) {
 
 $(function(){
     var timerSwitch = false;
-    var key, key_num, key_id;
+    var key, key_id;
     
     //在读取到键盘按键时触发
     $('#text').keydown(function (e) {
        
-       //在键盘按键首次摁下时，启动计时器
+       //在键盘按键首次摁下时，启动计时器，更新耗费时间和打字速度
         if(timerSwitch == false) {
             $('body').everyTime('1s', function(){
-                counter.inc();
+                counter.times += 1;
                 $("#times").text((counter.times).toString() + " 秒");               
                 $("#speed").text((counter.num * 60 / counter.times).toFixed(0).toString() + " KPM");                
             });
@@ -74,10 +66,9 @@ $(function(){
         }
         
         //根据按键字符更新成绩统计对象中的总字数和退格数
-        key_num = e.which;
-        update_counter(key_num, counter);
+        update_counter(e.which, counter);
         //更新文章中的index
-        update_articles(counter.num);
+        articles.index = counter.num - 1;
         
         //更新网页中的成绩统计
         $("#num").text(counter.num);
@@ -88,11 +79,18 @@ $(function(){
     //比较当前打字的字符是否与文章中对应字符相同
     $('#text').keypress(function(e) {
         key = String.fromCharCode(e.charCode);
+        //将回车键替换成空格键处理
+        if (e.charCode == 13) {
+            key = " ";
+        }
         key_id = "#" + counter.num;
         if(compare_key(key, articles.p_key())) {
             $(key_id).css("color","blue");
         } else {
             $(key_id).css("color","red");
+            //更新错字数，未完成!!
+            counter.errorNum += 1;
+            $("#errorNum").text(counter.errorNum);
         }
     });
 });
